1486a8c263de0931fa3dcec39cd097d68b1b596c
flood <teranys1986@gmail.com>
2023-05-29 19:29:11 +0300

    сделал модель юзера

--- skymarket/skymarket/urls.py
+++ skymarket/skymarket/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 # TODO здесь необходимо подклюючит нужные нам urls к проекту
 
 urlpatterns = [
+    path("api/token/", TokenObtainPairView.as_view(), name='token_obtain'),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name='token_obtain'),
     path("", include("users.urls")),
     path("api/admin/", admin.site.urls),
     path("api/redoc-tasks/", include("redoc.urls")),
